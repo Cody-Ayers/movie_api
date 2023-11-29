ec5d8d37d6b0552abc0c6d1031a4281763f923f7
Cody-Ayers <122771685+Cody-Ayers@users.noreply.github.com>
2023-11-28 22:47:11 -0700

    updated formating

--- auth.js
+++ auth.js
@@ -1,7 +1,7 @@
-const jwtSecret ='your_jwt_secret';
+const jwtSecret = 'your_jwt_secret';
 
 const jwt = require('jsonwebtoken'),
-passport = require('passport');
+    passport = require('passport');
 
 require('./passport'); // local passport file
 
@@ -9,25 +9,25 @@ let generateJWTToken = (user) => {
     return jwt.sign(user, jwtSecret, {
         subject: user.Email,
         expiresIn: '7d',
-        algorithm:'HS256'
+        algorithm: 'HS256'
     });
 }
 
 module.exports = (router) => {
-    router.post('/login', (req, res)=> {
-        passport.authenticate('local', { session: false }, (error, user, info)=> {
+    router.post('/login', (req, res) => {
+        passport.authenticate('local', { session: false }, (error, user, info) => {
             if (error || !user) {
-            return res.status(400).json({
-                message: 'Something is not right',
-                user: user
-            });
+                return res.status(400).json({
+                    message: 'Something is not right',
+                    user: user
+                });
             }
-            req.login(user, {session: false}, (error) => {
+            req.login(user, { session: false }, (error) => {
                 if (error) {
                     res.send(error);
                 }
                 let token = generateJWTToken(user.toJSON());
-                return res.json({ user, token});
+                return res.json({ user, token });
             });
         })(req, res);
     });
